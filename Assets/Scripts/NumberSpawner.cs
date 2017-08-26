14748cfa4eaca18e8449a80890418af312bde245
hlimbo <hlimbo@users.noreply.github.com>
2017-08-25 20:08:33 -0700

    Move random number generation in NumberSpawner.Scale boxcollider size to the preferred values of TextMeshPro Component (Box Collider resizes to fit the width of the number being randomly generated)

--- Assets/Scripts/NumberSpawner.cs
+++ Assets/Scripts/NumberSpawner.cs
@@ -10,6 +10,10 @@ public class NumberSpawner : MonoBehaviour {
     public int size;
     public GameObject numberPrefab;
     public float xOffset;
+    public float yOffset;
+
+    //for rng
+    public int min, max;
 
     private void Awake()
     {
@@ -25,51 +29,29 @@ public class NumberSpawner : MonoBehaviour {
             GameObject numberGO = Instantiate<GameObject>(numberPrefab, this.transform);
             numberGO.transform.localPosition = new Vector3((i * xOffset),0, 0);
             possibleAnswers.Add(numberGO);
+
         }
 
+        GeneratePossibleAnswers(0, 144);
+
+     
+
     }
 
-
-    float disableDuration = 1.0f;
-    float startTime;
-    float elapsedTime = 0.0f;
-    int index = -1;
-
-    // respawn number touched by player
-    //prob will have to write more coroutines for this one as well
-    void Update ()
+    //resizes width and height of text's box collider when given a random number
+    private void GeneratePossibleAnswers(int min,int max)
     {
-        //for (int i = 0; i < possibleAnswers.Count; ++i)
-        //{
-        //    index = -1;
-        //    if (!possibleAnswers[i].activeInHierarchy)
-        //    {
-        //        index = i;
-        //        startTime = Time.time;
-        //        elapsedTime = Time.time - startTime;
-        //        while (elapsedTime < disableDuration)
-        //        {
-        //            elapsedTime = Time.time - startTime;
-        //        }
+        foreach(GameObject numberGO in possibleAnswers)
+        {
+            TextMeshPro numberText = numberGO.GetComponent<TextMeshPro>();
+            BoxCollider2D numberBox = numberGO.GetComponent<BoxCollider2D>();
 
-        //        possibleAnswers[index].SetActive(true);
-        //        break;
-        //    }
-        //}
-
-        //if (index == -1)
-        //    startTime = Time.time;
-
-        //if (index != -1)
-        //{
-        //    elapsedTime = Time.time - startTime;
-        //    while (elapsedTime < disableDuration)
-        //    {
-        //        elapsedTime = Time.time - startTime;
-        //    }
-
-        //    possibleAnswers[index].SetActive(true);
-        //}
+            string randomNumber = Random.Range(min, max).ToString();
+            Vector2 boxColliderSize = numberText.GetPreferredValues(randomNumber);
+            numberBox.size = boxColliderSize;
+            numberText.SetText(randomNumber);
+            
+        }
     }
-   
+ 
 }
--- Assets/Scripts/OnTouchDisappear.cs
+++ Assets/Scripts/OnTouchDisappear.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+[RequireComponent(typeof(TMP_Text))]
 public class OnTouchDisappear : MonoBehaviour {
-
 
     private TMP_Text tmp_text;
 
@@ -13,27 +13,13 @@ public class OnTouchDisappear : MonoBehaviour {
         tmp_text = GetComponent<TMP_Text>();       
     }
 
-    private void Start()
-    {
-        int randomValue = Random.Range(0, 9);
-        tmp_text.text = randomValue.ToString();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name.Equals("Player"))
         {
             NumberEventManager.attempt_answer = tmp_text.text;
-            gameObject.SetActive(false);
+            //gameObject.SetActive(false);
         }
-    }
-
-    private void OnDisable()
-    {
-        //create another random value
-        int randomValue = Random.Range(0, 9);
-        tmp_text.text = randomValue.ToString();
-
     }
 
  
