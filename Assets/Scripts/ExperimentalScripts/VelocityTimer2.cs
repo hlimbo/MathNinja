288087eb5320883a73ca0ccb2ea9ef5dd599b1ec
hlimbo <hlimbo@users.noreply.github.com>
2017-08-31 22:31:43 -0700

    Experimenting with variable jump height

--- Assets/Scripts/ExperimentalScripts/VelocityTimer2.cs
+++ Assets/Scripts/ExperimentalScripts/VelocityTimer2.cs
@@ -21,13 +21,78 @@ using UnityEngine;
  */ 
 public class VelocityTimer2 : MonoBehaviour {
 
+    public float jumpHeight;
+    public float recordedJumpHeight;
+    public float jumpSpeed;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    private bool beginJump;
+    private float beginJumpY;
+    private float beginJumpTime;
+
+    public float timeElapsed;
+
+    private Vector2 jumpVelocity;
+    public float jumpTime;
+
 	// Use this for initialization
 	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
         Debug.Log("Physics2D gravity: " + Physics2D.gravity);
+        jumpSpeed = CalculateJumpVerticalSpeed(jumpHeight);
+        Debug.Log("Jump Speed: " + jumpSpeed);
+
+        jumpTime = Mathf.Sqrt(2 * jumpHeight / Mathf.Abs(Physics2D.gravity.y));
+        Debug.Log("jump Time: " + jumpTime);
+
+        jumpVelocity = new Vector2(0.0f, jumpSpeed);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!beginJump)
+            {
+                beginJump = true;
+                beginJumpY = rb.position.y;
+                beginJumpTime = Time.time;
+               
+            }
+        }
+
+        //fixed height jump
+        if(beginJump)
+        {
+            timeElapsed = Time.time - beginJumpTime;
+            float deltaY = rb.position.y - beginJumpY;
+            if(jumpTime / 2 > timeElapsed)//deltaY < jumpHeight)
+            {
+                //decelerate until jumpVelocity reaches 0
+                //rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.99f);
+                rb.velocity = new Vector2(rb.velocity.x, jumpVelocity.y);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, 0.0f);
+                Debug.Log("deltaY: " + deltaY);
+                beginJump = false;
+            }
+        }
 		
 	}
+
+    public float CalculateJumpVerticalSpeed(float targetJumpHeight)
+    {
+        // From the jump height and gravity we deduce the upwards speed 
+        // for the character to reach at the apex.
+        return Mathf.Sqrt(2f * targetJumpHeight * Mathf.Abs(Physics2D.gravity.y));
+    }
 }
