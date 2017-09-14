cda9868878d13354b62ac589fe2812016a91b07f
hlimbo <hlimbo@users.noreply.github.com>
2017-09-14 00:15:16 -0700

    Can press spacebar to play game again

---
+++ Assets/Scripts/playAgainButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class playAgainButton : MonoBehaviour {
+    void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
